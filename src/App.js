6ae4dabb4ddbaf20487fee71abf9411bf6866da3
Celesto-Fogue <boyomcelestin@gmail.com>
2021-01-12 23:27:51 +0100

    1.3.9

--- src/App.js
+++ src/App.js
@@ -4,8 +4,26 @@ import TodoList from './components/TodoList';
 
 function App() {
   return (
+<<<<<<< HEAD
     <div className='todo-app'>
       <TodoList />
+=======
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Mise en production 1.0 <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> parent of 948343e... 1.1
     </div>
   );
 }
